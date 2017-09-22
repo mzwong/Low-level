c2cce9302b859e5ee31eb0382efc638aa7083070
Maurice Wong <maurice.wong24@gmail.com>
2017-09-22 00:24:34 -0400

    Starting HW 2

---
+++ 2_sync/main.c
@@ -0,0 +1,25 @@
+/**
+ * Homework 2: Parallel, binary max function
+ *
+ * CS4414 Operating Systems
+ * Fall 2017
+ *
+ * Maurice Wong - mzw7af
+ *
+ * main.c - max function program
+ *
+ * COMPILE:     make
+ * OBJECTS:     main.o
+ * RUN:         ./max
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    int numbers[8192];
+    
+    return 0;
+}
