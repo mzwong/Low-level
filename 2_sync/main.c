33919e4993e2e2c45b57d6457fabc15cff1ad7a2
Maurice Wong <maurice.wong24@gmail.com>
2017-09-22 00:47:01 -0400

    Reading in numbers

--- 2_sync/main.c
+++ 2_sync/main.c
@@ -18,8 +18,34 @@
 #include <string.h>
 #include <unistd.h>
 
+int readNums(int* nums, int len);
+
 int main() {
-    int numbers[8192];
-    
+    int len = 8092;
+    int* nums = malloc(len * sizeof(int));
+    int numCount = readNums(nums, len);
+
+    int i = 0;
+    for (; i < numCount; i++) {
+        printf("%d", nums[i]);
+    }
     return 0;
+}
+
+/**
+ * Reads in numbers from stdin into nums until newline
+ * returns the # of numbers read in
+ */
+int readNums(int *nums, int len) {
+    char line[1024];
+    int numCount = 0;
+    while (fgets(line, 1024, stdin)[0] != '\n') {
+        if (numCount == len) {
+            len *= 2;
+            nums = realloc(nums, len * sizeof(int));
+        }
+        sscanf(line, "%d", &nums[numCount]);
+        numCount++;
+    }
+    return numCount;
 }
