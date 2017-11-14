d82bb252dc02ec19ed599055e285fee3ec3a58ef
Maurice Wong <maurice.wong24@gmail.com>
2017-11-14 01:21:22 -0500

    writeup

--- 3_4_fat/main.h
+++ 3_4_fat/main.h
@@ -1,16 +1,5 @@
 #include <stdint.h>
 
-typedef struct fat_extBS_16 {
-	//extended fat12 and fat16 stuff
-	unsigned char		bios_drive_num;
-	unsigned char		reserved1;
-	unsigned char		boot_signature;
-	unsigned int		volume_id;
-	unsigned char		volume_label[11];
-	unsigned char		fat_type_label[8];
-
-} __attribute__((packed)) fat_extBS_16_t;
-
 typedef struct fat_BS {
 	unsigned char 		bootjmp[3];
 	unsigned char 		oem_name[8];
--- 3_4_fat/test.c
+++ 3_4_fat/test.c
@@ -4,18 +4,26 @@
 
 int main() {
     OS_cd("MEDIA");
-    int fd = OS_open("404-FAILJPG");
+    int fd = OS_open("/PEOPLE/ABK2Y/COFFEE~1TXT");
+    int fd2 = OS_open("HEARSE~1JPG");
     char buffer[500000];
-    int bytes_read = OS_read(fd, buffer, 5000000, 0);
+    char buffer2[500000];
+    int bytes_read = OS_read(fd, buffer, 5000000, 10);
+    int bytes_read2 = OS_read(fd2, buffer2, 5000000, 0);
+
+
     printf("%s\n", buffer);
     printf("%d\n", bytes_read);
 
     // writing buffer to a file
     FILE *fp;
-    fp = fopen("fail.jpg", "w+");
+    fp = fopen("fail.txt", "w+");
     fwrite(buffer, 100000, 1, fp);
 
-    dirEnt* directories = OS_readDir("../MEDIA");
+    fp = fopen("fail2.jpg", "w+");
+    fwrite(buffer2, 100000, 1, fp);
+
+    dirEnt* directories = OS_readDir("../PEOPLE/ABK2Y");
     if (directories == NULL) {
         printf("NULL FOUND YAY\n");
         return 0;
@@ -28,6 +36,8 @@ int main() {
     OS_cd("ABK2Y/");
     OS_cd("/");
     OS_cd("PEOPLE/ABK2Y");
+    printf("file close %d\n", OS_close(fd));
+    printf("file close %d\n", OS_close(fd2));
     printf("file close %d\n", OS_close(fd));
 
 }
